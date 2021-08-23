59596e3ec21c8a84fcfa26072a26345339c863d1
SvenSvld <84246081+SvenSvld@users.noreply.github.com>
2021-08-23 22:18:27 +0200

    Add files via upload

---
+++ les_1.py
@@ -0,0 +1,112 @@
+12+1
+36-4
+18*2
+18/2
+25.0/4
+100/4
+
+print("Hello, world!")
+
+hoofdpersonage = "Harry Potter"
+rugnummer = 7
+print(rugnummer)
+print(hoofdpersonage)
+
+getal_1 = 10
+getal_2 = 20
+som = getal_1 + getal_2
+print(som)
+
+input("Wat is jouw voornaam?")
+voornaam = input("Wat is jouw voornaam?")
+#geretourneerd antwoord is de inhoud van de variabele 'voornaam'
+
+
+voornaam = input("Wat is jouw voornaam?")
+achternaam = input("Wat is jouw achternaam?")
+volledige_naam = voornaam + " " + achternaam
+
+getal = "8"
+int(getal)
+getal = int(getal)
+
+
+
+getal_1_als_string = input("Wat is uw eerste getal?")
+getal_1 = int(getal_1_als_string)
+
+getal_2_als_string = input("Wat is uw tweede getal?")
+getal_2 = int(getal_2_als_string)
+
+som = getal_1 + getal_2
+print(som)
+
+# korter schrijven
+getal_1 = int(input("Wat is uw eerste getal?"))
+getal_2 = int(input("Wat is uw tweede getal?"))
+som = getal_1 + getal_2
+print(som)
+
+"7" + "79"
+7+79
+
+type("Hoi")
+type(15/7)
+type(2)
+type(int("7"))
+
+
+getal_als_getal = 7
+type(getal_als_getal)
+
+str(getal_als_getal)
+
+getal_als_string = str(getal_als_getal)
+type(getal_als_string)
+
+
+#Rekeningsaldo voorbeeld
+Rekeningsaldo = 123
+zin = "Het saldo op je rekening bedraagt" + str(Rekeningsaldo) + "euro!"
+print(zin)
+
+#Rekeningsaldo voorbeeld
+Rekeningsaldo = 456
+zin = "Het saldo op je rekening bedraagt" + str(Rekeningsaldo) + "euro!"
+print(zin)
+
+5**5
+5*5*5*5*5
+
+9**(1/2)
+
+print("Lukaku")
+print("Hazard")
+print("Mertens")
+
+print("Lukaku","Hazard","Mertens")
+
+speler_1 = "Lukaku"
+speler_2 = "Hazard"
+speler_3 = "Mertens"
+
+print(speler_1, speler_2, speler_3)
+
+getal = '1.3'
+print(getal)
+
+float(getal)
+
+getal = float(getal)
+
+
+
+
+getal = 100
+
+if getal > 5:
+    print("Amai,")
+    print("dat is echt een groot getal")
+    print("Allez, ik bedoel: 'het is toch groter dan 5!' ")
+
+
