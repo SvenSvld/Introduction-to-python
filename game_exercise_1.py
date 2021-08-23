979342746085109b76a16be684d707dd16217ac6
SvenSvld <84246081+SvenSvld@users.noreply.github.com>
2021-08-23 23:12:32 +0200

    Add files via upload

--- game_exercise_1.py
+++ game_exercise_1.py
@@ -5,6 +5,9 @@ score = 0
 total_q = 2
 
 if ans.lower() == 'yes':
+    ##ans.lower zorgt ervoor dat alle input die de gebruiker typt
+    ##wordt omgezet in een string. Dus if 'ben je klaar om te spelen'?
+    ##yes > laat het programma draaien...
     ans = input('1. What is 1+1?')
     if ans == '2':
         score += 1
